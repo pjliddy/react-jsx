4a6f222320cd06edbf2f50d9d1307067894407b6
pjliddy <pjliddy@gmail.com>
2019-04-28 17:54:32 -0400

    Sample code for Udemy course

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
+// import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// define component functions
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// function getButtonText() {
+//   return 'Button Text';
+// }
+
+// create a function-based React component
+
+const App = function() {
+  const buttonText = {
+    text: 'Click Me'
+  };
+
+  const style = {
+    backgroundColor: 'blue',
+    color: 'white'
+  };
+
+  return (
+    <div>
+      <label htmlFor="name">Name:</label>
+      <input id="name"/>
+      <button style={style}>{buttonText.text}</button>
+    </div>
+  );
+}
+
+// take the react component and display in browser
+ReactDOM.render(
+  <App/>, document.querySelector('#root'));
